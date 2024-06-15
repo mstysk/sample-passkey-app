82e56251828577005f8dba589547b22e4479dad2
Masato Yoshioka <yoshioka0627@gmail.com>
2024-06-16 07:32:11 +0900

    feat(routes): 新規にindex routeとログインフォームを追加
    body: routesディレクトリに新たに`index.tsx`ファイルを追加しました。このファイルは、
    ランダムなユーザー名でログインするためのフォームを返すデフォルト関数をエクスポートします。
    フォームには、ユーザー名のテキスト入力と送信ボタンが含まれています。

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,6 +4,7 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
+import * as $index from "./routes/index.tsx";
 
 import { type Manifest } from "$fresh/server.ts";
 
@@ -11,6 +12,7 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
+    "./routes/index.tsx": $index,
   },
   islands: {},
   baseUrl: import.meta.url,
---
+++ routes/index.tsx
@@ -0,0 +1,12 @@
+export default function Index() {
+  return (
+    <>
+      <h2>Login with a random username</h2>
+      <form>
+        <label>username</label>
+        <input type="text" name="username" />
+        <input type="submit" value="Next" />
+      </form>
+    </>
+  );
+}
