cdb3d7a7c6667ea1bfd63c3735b08bb9aca0b86d
Masato Yoshioka <yoshioka0627@gmail.com>
2024-06-16 09:40:14 +0900

    style(routes/index.tsx): フォームのスタイルを改善
    body: routes/index.tsxのフォームを更新して見た目を改善しました。
    ユーザー名の入力フィールドと送信ボタンは、ライトモードとダークモードの両方で
    より良いスタイルを持つようになりました。また、フォームは中央に配置され、
    最大幅も設定されています。

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,10 +2,27 @@ export default function Index() {
   return (
     <>
       <h2>Login with a random username</h2>
-      <form>
-        <label>username</label>
-        <input type="text" name="username" />
-        <input type="submit" value="Next" />
+      <form class="max-w-sm mx-auto">
+        <div class="mb-5">
+          <label
+            for="username"
+            class="block mb-2 text-sm font-mdeium text-gray-900 dark:text-white"
+          >
+            username
+          </label>
+          <input
+            type="text"
+            name="username"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Next
+        </button>
       </form>
     </>
   );
